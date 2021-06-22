90a9a1324efd548708b01d95fa7099f59bbcef9c
Joseph Rouff <rouffj@gmail.com>
2021-06-22 15:56:14 +0200

    Add review dynamically

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -38,6 +38,7 @@ class AppFixtures extends Fixture
 
         $movie1 = $manager->getRepository(Movie::class)->findOneBy(['title' => 'Memento']);
         $user1 = $manager->getRepository(User::class)->findOneBy(['email' => 'joseph@joseph.io']);
+        $user2 = $manager->getRepository(User::class)->findOneBy(['email' => 'omar@sy.io']);
 
         $review = new Review();
         $review
@@ -46,8 +47,16 @@ class AppFixtures extends Fixture
             ->setMovie($movie1)
             ->setUser($user1)
         ;
+        $review2 = new Review();
+        $review2
+            ->setRating(2)
+            ->setContent("Quisque sit amet enim augue. Pellentesque iaculis, enim at efficitur malesuada, elit odio ullamcorper ante, vel scelerisque nibh diam at elit. Donec eget purus arcu. Nam blandit, massa gravida vehicula sagittis, justo ex pretium nunc, et tempor nisi eros quis velit. Aliquam ullamcorper metus a ligula tristique eleifend. Phasellus id rutrum massa. Donec ullamcorper suscipit risus. Mauris nec odio elit. In quis ipsum at leo venenatis pharetra commodo nec diam. Aenean fringilla odio tortor, placerat rutrum orci gravida nec.")
+            ->setMovie($movie1)
+            ->setUser($user2)
+        ;
 
         $manager->persist($review);
+        $manager->persist($review2);
 
         $manager->flush();
     }
