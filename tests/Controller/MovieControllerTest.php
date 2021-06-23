fc198028294fa77021b684ac83f3ed186e8c3a40
Joseph Rouff <rouffj@gmail.com>
2021-06-23 09:54:42 +0200

    Add tests

--- tests/Controller/MovieControllerTest.php
+++ tests/Controller/MovieControllerTest.php
@@ -9,9 +9,15 @@ class MovieControllerTest extends WebTestCase
     public function testSomething(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/movie/22'); // Inception
 
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
+        $this->assertSelectorTextContains('h1', 'Inception');
+
+        $reviewSelector = '.row.p-sm-5';
+        $this->assertEquals(0, $crawler->filter($reviewSelector)->count());
+
+        $crawler = $client->request('GET', '/movie/19'); // Memento
+        $this->assertEquals(2, $crawler->filter($reviewSelector)->count(), 'Memento movie should have only 2 reviews');
     }
 }
